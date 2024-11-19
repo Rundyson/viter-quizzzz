de2502a029d201991d4ab9cb881860080c353e09
Mjcapunitan <mjbcapunitan@gmail.com>
2024-11-19 08:02:39 +0800

    first

---
+++ src/Questions.jsx
@@ -0,0 +1,112 @@
+export const questions = [
+    {
+        question_aid: 1,
+        question_question: "Q1: Maganda ba ako?",
+        choices:[
+            {
+                choice: "Pwede",
+                isCorrect: false,
+            },
+            {
+                choice: "Oo",
+                isCorrect: true,
+            },
+            {
+                choice: "Siguro",
+                isCorrect: false,
+            },
+            {
+                choice: "Hindi",
+                isCorrect: false,
+            },
+        ],
+    },
+    {
+        question_aid: 2,
+        question_question: "Q2: Papasa ba ako?",
+        choices:[
+            {
+                choice: "Pwede",
+                isCorrect: false,
+            },
+            {
+                choice: "Oo",
+                isCorrect: true,
+            },
+            {
+                choice: "Siguro",
+                isCorrect: false,
+            },
+            {
+                choice: "Asa ka pa",
+                isCorrect: false,
+            },
+        ],
+    },
+    {
+        question_aid: 3,
+        question_question: "Q3: Mahal ba ako?",
+        choices:[
+            {
+                choice: "Pwede",
+                isCorrect: false,
+            },
+            {
+                choice: "Oo",
+                isCorrect: false,
+            },
+            {
+                choice: "Siguro",
+                isCorrect: true,
+            },
+            {
+                choice: "Hindi",
+                isCorrect: false,
+            },
+        ],
+    },
+    {
+        question_aid: 4,
+        question_question: "Q4: Papasok ba ako?",
+        choices:[
+            {
+                choice: "Pwede",
+                isCorrect: false,
+            },
+            {
+                choice: "Oo",
+                isCorrect: false,
+            },
+            {
+                choice: "Siguro",
+                isCorrect: true,
+            },
+            {
+                choice: "Hindi",
+                isCorrect: false,
+            },
+        ],
+    },
+    {
+        question_aid: 5,
+        question_question: "Q5: Tao ba ako?",
+        choices:[
+            {
+                choice: "Pwede",
+                isCorrect: false,
+            },
+            {
+                choice: "Oo",
+                isCorrect: false,
+            },
+            {
+                choice: "Siguro",
+                isCorrect: false,
+            },
+            {
+                choice: "Hindi",
+                isCorrect: true,
+            },
+        ],
+    },
+]
