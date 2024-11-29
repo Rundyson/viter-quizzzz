13b92667af8e0f25037e0d628a4892213c420f1c
Mjcapunitan <mjbcapunitan@gmail.com>
2024-11-29 15:46:21 +0800

    update

---
+++ src/components/pages/backend/question/Question.jsx
@@ -0,0 +1,53 @@
+import { Plus } from 'lucide-react'
+import React from 'react'
+import SideNavigation from '../partials/SideNavigation'
+import Header from '../partials/Header'
+import SearchBar from '../partials/Searchbar'
+import Footer from '../partials/Footer'
+import Table from './QuestionTable'
+import { StoreContext } from '@/components/store/storeContext'
+import { setIsAdd } from '@/components/store/storeAction'
+import ModalAddQuestion from './ModalAddQuestion'
+import ToastSuccess from '../partials/ToastSuccess'
+import ModalError from '../partials/modals/ModalError'
+import ModalValidation from '../partials/modals/ModalValidation'
+
+
+const  Question = () => {
+    const {dispatch, store} = React.useContext(StoreContext);
+    const [itemEdit, setItemEdit] = React.useState(null);
+
+    const handleAdd = () => {
+      dispatch(setIsAdd(true));
+      setItemEdit(null);
+    }
+  return (
+    <>
+    <section className="layout-main">
+            <div className="layout-division">
+            <SideNavigation menu="question"/>
+                <main>
+                    <Header title="Question" subtitle="Manage Question"/>
+                    <div className="p-8">
+                      <div className="flex justify-between items-center">
+                        <SearchBar/>
+                        <button className="btn btn-add" onClick={handleAdd}>
+                          <Plus size={16}/> Add New
+                        </button>
+                      </div>
+                      <Table setItemEdit={setItemEdit}/>
+                    </div>
+                    <Footer/>
+                </main>
+            </div>
+        </section>
+        {store.validate && <ModalValidation/>}
+        {store.error && <ModalError/>}
+      {store.success && <ToastSuccess/>}
+        {/* <SpinnerWindow/> */}
+        {store.isAdd && <ModalAddQuestion itemEdit={itemEdit}/>}
+    </>
+  )
+}
+
+export default Question
